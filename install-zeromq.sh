88bfc528ebae2e6ca8f8734ec893c6a1090c6922
Martin Löper <martin.loeper@nesto-software.de>
2021-01-25 02:08:10 +0000

    feat: fix zeromq installation

--- install-zeromq.sh
+++ install-zeromq.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# Before installing, make sure you have installed all the needed packages
-sudo apt-get install libtool pkg-config build-essential autoconf automake
-sudo apt-get install libzmq-dev
-
-# Install libsodium
-git clone git://github.com/jedisct1/libsodium.git
-cd libsodium
-./autogen.sh
-./configure && make check
-sudo make install
-sudo ldconfig
-
-cd /opt
-
-# Install zeromq
-# latest version as of this post is 4.1.2
-wget http://download.zeromq.org/zeromq-4.1.2.tar.gz
-tar -xvf zeromq-4.1.2.tar.gz
-cd zeromq-4.1.2
-./configure
-make
-sudo make install
+# is following needed?
+#echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
+#wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+apt-get install libzmq3-dev libzmq-dev
