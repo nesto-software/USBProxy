ec7ffa4a9a1419a929d9cd226a3d9aeffee462c4
Martin Löper <martin.loeper@nesto-software.de>
2021-01-25 05:49:48 +0000

    feat: add the endpoint

--- src/Plugins/Filters/PacketFilter_ZeroMQ.cpp
+++ src/Plugins/Filters/PacketFilter_ZeroMQ.cpp
@@ -3,6 +3,12 @@
 #include <zmq.hpp>
 #include <msgpack.hpp>
 
+struct ZMQ_MSG {
+	std::vector<__u8> d;
+	__u8 e;
+	MSGPACK_DEFINE_MAP(d, e);
+};
+
 PacketFilter_ZeroMQ::PacketFilter_ZeroMQ(ConfigParser *cfg) {
     ctx = new zmq::context_t();
     sock = new zmq::socket_t(*ctx, zmq::socket_type::pub);
@@ -17,14 +23,12 @@ PacketFilter_ZeroMQ::~PacketFilter_ZeroMQ() {
 
 void PacketFilter_ZeroMQ::filter_packet(Packet* packet) {
 	if (packet->wLength<=64) {
-		char* hex=hex_string((void*)packet->data,packet->wLength);
-		printf("%02x[%d]: %s\n",packet->bEndpoint,packet->wLength,hex);
-
-		const std::string m = std::string(hex);
-		std::vector<__u8> payload(packet->data, packet->data + packet->wLength);
+		struct ZMQ_MSG msg;
+		msg.d = std::vector<__u8>(packet->data, packet->data + packet->wLength);
+		msg.e = packet->bEndpoint;
 
 		std::stringstream buffer;
-		msgpack::pack(buffer, payload);
+		msgpack::pack(buffer, msg);
 		buffer.seekg(0);
 		std::string str(buffer.str());
 
@@ -35,7 +39,7 @@ void PacketFilter_ZeroMQ::filter_packet(Packet* packet) {
 }
 
 void PacketFilter_ZeroMQ::filter_setup_packet(SetupPacket* packet,bool direction) {
-
+	// TODO: determine if the setup packet is relevant for us
 }
 
 static PacketFilter_ZeroMQ *proxy;
