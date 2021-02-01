df4c48bd966afe8a4c9b9e14d04becd044780fb1
Martin Löper <martin.loeper@nesto-software.de>
2021-02-01 03:56:05 +0100

    fix: libusb-1.0.

--- docker-crosstool-ng-arm/build-binary.sh
+++ docker-crosstool-ng-arm/build-binary.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# ARG: $1 (CONTINUE_ON_ERROR): set to anything to continue if docker build failed
-# ARG: $2 (KEEP_BUILD_CONTAINER_RUNNING): set to anything to build and jump into the container afterwards
-# ARG: $3 (TRANSFER_TO_PI_SSH_KEYFILE_PATH): set to ssh key which is used to upload the artifact on a rpi
+# ARG: $1 (KEEP_BUILD_CONTAINER_RUNNING): set to 1 to build and jump into the container afterwards
+# ARG: $2 (TRANSFER_TO_PI_SSH_KEYFILE_PATH): set to ssh key which is used to upload the artifact on a rpi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -17,7 +16,8 @@ rm -Rf "${DIR}/bin/"*
 rm usb-mitm.tar
 
 docker build -t ${IMAGE_NAME} .
-if [ $? -ne 0 ] && [ -z "$1" ]; then
+if [ $? -ne 0 ]; then
+  echo -e "\nWe MUST abort because docker build already failed and no new image was created!\n"
   exit 1;
 fi
 
@@ -25,7 +25,7 @@ fi
 # see: https://stackoverflow.com/questions/25292198/docker-how-can-i-copy-a-file-from-an-image-to-a-host
 
 # use the following to debug the container after successful docker build
-if [ ! -z "$2" ]; then
+if [ ! -z "$1" ] && [[ ( "$1" == 1 ) ]]; then
     echo -e "\nCommands to run in another shell:\n---------------------------------\nConnect: docker exec -it crosstool-builder bash\nKill: docker kill crosstool-builder\n"
     docker run --rm --name "${CONTAINER_NAME}" "${IMAGE_NAME}" tail -f /dev/null; exit
 else
@@ -36,7 +36,7 @@ else
     docker kill "${CONTAINER_NAME}"
 fi
 
-if [ ! -z "$3" ]; then
+if [ ! -z "$2" ]; then
     scp -i "$3" ./usb-mitm.tar  pi@169.254.100.1:/home/pi/usb-mitm.tar
 fi
 
