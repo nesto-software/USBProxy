9cafc4683ded34ce2d77edcf0fff4513f9c406bf
Martin Löper <martin.loeper@nesto-software.de>
2021-02-14 02:10:08 +0100

    chore: release v0.0.3

--- .github/create-release.sh
+++ .github/create-release.sh
@@ -14,7 +14,17 @@ echo "Please note: If you did not use this script before, please run ./install-r
 BUMP="${1:-patch}"
 VERSION=$(cat "${DIR}/../VERSION")
 NEW_VERSION=$(semver bump "$BUMP" "$VERSION")
+
+if [ -z "$DEBFULLNAME" ]; then
+	export DEBFULLNAME=`git log -n 1 --pretty=format:%an`
+fi
+
+if [ -z "$DEBEMAIL" ]; then
+	export DEBEMAIL=`git log -n 1 --pretty=format:%ae`
+fi
+
+
 (cd "${DIR}/../src" && dch -v ${NEW_VERSION} --distribution main --force-distribution)
-echo "$NEW_VERSION" > "${DIR}/../VERSION"
+echo -n "$NEW_VERSION" > "${DIR}/../VERSION"
 
 echo "You are ready to commit the new version: git add -A && git commit -m \"chore: release v${NEW_VERSION}\""
