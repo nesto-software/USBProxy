22197fabea8cce76eb71edcab5fe3ed9679b7021
Martin Löper <martin.loeper@nesto-software.de>
2021-02-14 21:20:41 +0100

    fix: missing cmake dep

--- rpi-scripts/install-all-dependencies.sh
+++ rpi-scripts/install-all-dependencies.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-echo "[1/5] Installing libusb"
+echo -e "[0/6] Updating package list\n"
+sudo apt-get update
+
+echo -e "[1/6] Installing cmake"
+sudo apt-get install -y cmake
+
+echo -e "[2/6] Installing libusb\n"
 sudo apt-get install -y libusb-1.0.0-dev
 
-echo "[2/5] Installing boost"
+echo -e "[3/6] Installing boost\n"
 sudo apt-get install -y libboost-all-dev
 
-echo "[3/5] Installing libzmq"
+echo -e "[4/6] Installing libzmq\n"
 git clone https://github.com/zeromq/libzmq.git /tmp/libzmq
 (cd /tmp/libzmq/ && /tmp/libzmq/autogen.sh; ./configure; make; sudo make install)
 
-echo "[4/5] Installing cppzmq"
+echo -e "[5/6] Installing cppzmq\n"
 git clone https://github.com/zeromq/cppzmq.git /tmp/cppzmq
 (cd /tmp/cppzmq && mkdir build && cd build && cmake .. && sudo make -j4 install)
 
-echo "[5/5] Installing msgpack"
+echo - e"[6/6] Installing msgpack\n"
 sudo apt-get install -y doxygen
 git clone https://github.com/msgpack/msgpack-c.git /tmp/msgpack-c
 (cd /tmp/msgpack-c && git checkout cpp_master && cmake -DMSGPACK_CXX17=ON . && sudo make install)
