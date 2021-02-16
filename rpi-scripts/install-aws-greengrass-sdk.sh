60264f80907b361545664c500b6728bd7dca0d91
Martin Loeper <martin.loeper@nesto-software.de>
2021-02-16 02:05:36 +0000

    fix: DESTPATH for aws-greengrass-core-sdk

--- rpi-scripts/install-aws-greengrass-sdk.sh
+++ rpi-scripts/install-aws-greengrass-sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/aws/aws-greengrass-core-sdk-c.git /tmp/aws-sdk
-(cd /tmp/aws-sdk/ && git checkout a71613711438b48588e177f86ab322dd3992e780 && cd ./aws-greengrass-core-sdk-c && mkdir -p build && cd build && cmake .. && cmake --build . && sudo make install)
+(cd /tmp/aws-sdk/ && git checkout a71613711438b48588e177f86ab322dd3992e780 && cd ./aws-greengrass-core-sdk-c && mkdir -p build && cd build && cmake .. && cmake --build . && sudo make install DESTDIR=$1)
