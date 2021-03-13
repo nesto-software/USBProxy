6a67c877f884dd43339bb043a3614705e04217d6
Martin Löper <martin.loeper@nesto-software.de>
2021-03-14 00:51:19 +0100

    feat: add a delimiter example

---
+++ nodejs-client/delimiter_example.js
@@ -0,0 +1,44 @@
+const zmq = require("zeromq");
+const msgpack = require('msgpack5')();
+
+const sock = new zmq.Subscriber;
+
+(async function() {
+    sock.connect("tcp://127.0.0.1:5678");
+    sock.subscribe();
+    console.log("Subscriber connected to port 5678");
+    let buf = [];
+    let matched = 0;
+    let desired = [0x1b, 0x6d];
+    for await (const [msg] of sock) {
+       for(const c of msg) {
+	  buf.push(c);
+          const expected = desired[matched];
+          if(expected == c) {
+            matched++;
+	  } else {
+            matched = 0;
+          }
+
+          if(matched == 2) {
+	    console.log("CUT found. Receipt:\n");
+	    console.log(buf.map(s => String.fromCharCode(s)).join(""));
+	    matched = 0;
+            buf = [];
+	  }
+	  //console.log(c.toString(16));
+          //console.log(c.toString());
+	  //if(buf.length === 100) {
+	    //console.log(buf.map(s => s.toString(16)));
+            //console.log(buf.map(s => String.fromCharCode(s)).join(""));
+            //console.log(Buffer.from(buf));
+	    //buf = [];
+          //}
+       }
+
+       //console.log(buf.map(s => s.toString(16)));
+       //console.log(buf.map(s => String.fromCharCode(s)).join(""));
+       // const msg_decoded = msgpack.decode(msg);
+       // console.log(msg_decoded.d);
+    }
+})();
