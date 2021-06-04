0798281522faf98125474c98297c2792172385f0
Martin Löper <martin.loeper@nesto-software.de>
2021-06-05 00:47:59 +0200

    fix: remove package versions

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install -y libzmq3-dev=4.3.1-4+deb10u2 libusb-1.0-0=2:1.0.22-2 libudev1=241-7~deb10u5+rpi1 coreutils sed
+apt-get install -y libzmq3-dev libusb-1.0-0 libudev1 coreutils sed
